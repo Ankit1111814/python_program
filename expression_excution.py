07be9c82fd89e3c2fb8585f20db71f33ba1aaa6a
Ankit <ankiyqq22@gmail.com>
2025-08-01 13:59:55 +0530

    Expression execution

---
+++ expression_excution.py
@@ -0,0 +1,11 @@
+A,B = "2",3
+text ="@"
+print((A+text)*B)
+
+
+# This code snippet is a simple demonstration of string concatenation and multiplication in Python.
+# It takes a string "2", an integer 3, and a character "@", then
+# concatenates the string with the character and multiplies the result by the integer.# The output will be "@@@" since "2" is treated as a string and concatenated
+# with "@" three times.# to produce the final output.
+# The output will be "@@@" since "2" is treated as a string and concatenated
+# with "@" three times
