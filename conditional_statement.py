4b62fc1df8858ecfbd8aec25b23445826beaffb3
Ankit <ankiyqq22@gmail.com>
2025-08-01 15:09:33 +0530

    Conditional statements

---
+++ conditional_statement.py
@@ -0,0 +1,11 @@
+light =input("Enter a light : ")
+if(light == "red"):
+  print("Stop")
+elif(light == "green"):
+  print("Go")
+else:
+  print("Caution")
+
+
+# This code snippet checks the light condition and prints a message based on the input.
+# If the light is red, it prints "Stop"; if green, it prints "Go"; otherwise, it prints "Caution".
