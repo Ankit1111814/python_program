c1fcb1004648306f9a28afd811995898ddb65672
Ankit <ankiyqq22@gmail.com>
2025-08-01 14:48:03 +0530

    Input from user

---
+++ input.py
@@ -0,0 +1,8 @@
+a=input("Enter a name: ")
+b=int(input("Enter a number: "))
+print ("name is", a, "and number is", b)
+
+# This code snippet prompts the user to enter a name and a number,
+# then prints the name and number in a formatted string.
+# The output will display the entered name and number.
+  
