1f7831ccff104d8d9e868c62e1346228fadab7d7
Ankit <ankiyqq22@gmail.com>
2025-08-02 23:40:45 +0530

    searching in tuple

---
+++ searching.py
@@ -0,0 +1,10 @@
+nums= (78,43,23,54,23,133,445,23,124,34,23,12,34,56,78,90,12,34,56,78,90)
+x=34
+i =0
+while i < len(nums):
+  if nums[i] == x:
+    print ("found at index",i)
+
+  else:
+    print ("not found at index")
+  i += 1
