b7597a4c4c40b413a4e6d4997e10017f9fff58bf
Ankit <ankiyqq22@gmail.com>
2025-08-02 15:18:38 +0530

    while loop

---
+++ while_loop.py
@@ -0,0 +1,9 @@
+count =1
+while count <=23:
+  print("ankit ")
+  count += 1
+print("Loop finished")
+print("Total iterations:", count - 1)
+print("End of program")
+print("Thank you for using the program!")
+print("Have a great day!")
