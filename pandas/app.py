bf7ee91edfcc8caf81737ff1964c88020fd40aec
Ankit <ankiyqq22@gmail.com>
2025-08-04 23:07:59 +0530

    read excel

--- pandas/app.py
+++ pandas/app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 #df =pd.read_csv("sales_data_sample.csv",encoding ="latin1")
-df = pd.read_json("sample_Data.json")
+#df = pd.read_json("sample_Data.json")
+df = pd.read_excel("SampleSuperstore.xlsx")
 print(df)
