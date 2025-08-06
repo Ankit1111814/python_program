c584b5278722962247904293ccbf2c98e8a923e7
Ankit <ankiyqq22@gmail.com>
2025-08-06 22:21:01 +0530

    change

---
+++ pandas/subset.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data ={
+  "Name":["Ankit", "Ankita", "Amit", "Aman", "Anjali", "Anshul",  "Ankush", "Ananya", "Aniket", "Anushka",  "Anirudh", "Anjali"],
+  "Age":[23, 22, 24, 25, 21, 26, 27, 28, 29, 30, 31, 32],
+  "City":["Delhi", "Mumbai", "Bangalore", "Chennai", "Kolkata", "Hyderabad", "Pune", "Ahmedabad", "Jaipur", "Lucknow", "Kanpur", "Nagpur"]
+}
+
+df =  pd.DataFrame(data)
+print(df)
+name = df[name]
+print(name)
