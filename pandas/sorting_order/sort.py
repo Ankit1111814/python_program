06fffa15ba69522f99c8e071d6bda8acec3baa6d
Ankit <ankiyqq22@gmail.com>
2025-08-07 16:55:23 +0530

    sort the value  in ascending order

---
+++ pandas/sorting_order/sort.py
@@ -0,0 +1,15 @@
+import pandas as pd
+data = {
+      "Name":["Alice", None, "Charlie", "David", "Eve", "Frank"],
+      "Age":[24, None, 22, 32, 29,45],
+      "City":["New York", None, "Chicago", "Houston", "Phoenix", "Philadelphia"],
+      "Salary":[70000, None, 60000, 90000, 85000, 95000],
+      "Department":["HR", None, "IT", "Marketing", "Sales", "Operations"]
+}
+
+df = pd.DataFrame(data)
+
+
+df.sort_values(by="Age", ascending=True, inplace=True)
+print("sorting the by age")
+print(df)
