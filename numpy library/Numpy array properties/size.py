68f7a857c4c9f1028d9dcc23ecb45f944f0c9ea0
Ankit <ankiyqq22@gmail.com>
2025-08-09 23:43:34 +0530

    size

---
+++ numpy library/Numpy array properties/size.py
@@ -0,0 +1,6 @@
+# size()
+
+import numpy as np
+arr = np.array([[2, 4, 3], [3, 2, 1], [1, 2, 3]])  # Creates a 2D array
+print(arr.size)  # Prints the total number of elements in the array
+print(arr)  # Displays the array
