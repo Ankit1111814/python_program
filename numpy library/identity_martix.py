b4fe8f159c4ed45d14ccf04b2d045bade5f1d6ad
Ankit <ankiyqq22@gmail.com>
2025-08-09 23:18:28 +0530

    identity martix

---
+++ numpy library/identity_martix.py
@@ -0,0 +1,14 @@
+# creating an identity matrix using numpy
+# eye(shape) creates an identity matrix of specified shape
+
+import numpy as np
+identity_matrix = np.eye(3)  # Creates a 3x3 identity matrix
+print(identity_matrix)
+
+
+
+# Output:
+# [[1. 0. 0.]
+#  [0. 1. 0.]
+#  [0. 0. 1.]]
+
