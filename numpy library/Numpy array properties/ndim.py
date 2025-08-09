2f25f8c00fa780fbd272c04879b3306e5d8ba285
Ankit <ankiyqq22@gmail.com>
2025-08-09 23:48:28 +0530

    ndim

---
+++ numpy library/Numpy array properties/ndim.py
@@ -0,0 +1,10 @@
+# ndim find the dimension of the array
+
+import numpy as np
+arr1=np.array([2,4,5]) # Creates a 1D array
+arr2= np.array([[2, 4, 3], [3, 2, 1], [1, 2, 3]])
+print(arr1.ndim)  # Prints the number of dimensions of the array
+print(arr2.ndim)  # Prints the number of dimensions of the array
+print(arr1)  # Displays the 1D array
+print(arr2)  # Displays the 2D array
+#  --- IGNORE ---
