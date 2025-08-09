3e9e8cf3501c9408393e08592f5ee0b1e936d6c5
Ankit <ankiyqq22@gmail.com>
2025-08-09 23:07:06 +0530

    fill with ones

---
+++ numpy library/fill_ones.py
@@ -0,0 +1,6 @@
+# np.ones(shape) creates an array filled with ones
+# shape (3) 1d, (3, 3) 2d, (3, 3, 3) 3d
+
+import numpy as np
+arr =np.ones((3,3)) # Creates a 2D array of shape (3, 3) filled with ones
+print(arr)
