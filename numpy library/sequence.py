33f4e72bf3b8f3de3de7e19eb29fb12bf2bcdb96
Ankit <ankiyqq22@gmail.com>
2025-08-09 23:13:07 +0530

    arange function for sequence

---
+++ numpy library/sequence.py
@@ -0,0 +1,6 @@
+# np.arrange(start, stop, step) creates an array with a range of values
+# start (0) default start value, stop (10) exclusive end value, step (1) default step value
+
+import numpy as np
+arr= np.arange(0, 10, 1)  # Creates a 1D array with values from 0 to 9
+print(arr)
