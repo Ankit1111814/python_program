482faa305038523be416cc92e3b13d77b6878952
Ankit <ankiyqq22@gmail.com>
2025-08-09 23:03:29 +0530

    # np.zeroes(shape) creates an array filled with zeros # np.ones(shape) creates an array filled with ones # shape (3) 1d, (3, 3) 2d, (3, 3, 3) 3d

---
+++ numpy library/default_array.py
@@ -0,0 +1,8 @@
+# np.zeroes(shape) creates an array filled with zeros
+# np.ones(shape) creates an array filled with ones
+# shape (3) 1d, (3, 3) 2d, (3, 3, 3) 3d
+
+
+import numpy as np
+arr =np.zeros((3, 3))  # Creates a 2D array of shape (3, 3) filled with zeros
+print(arr)
