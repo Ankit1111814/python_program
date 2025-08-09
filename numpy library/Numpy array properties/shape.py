6187939ddfbf25981028d89ec820e46c3b6fafd0
Ankit <ankiyqq22@gmail.com>
2025-08-09 23:34:34 +0530

    shape for find the row and column

---
+++ numpy library/Numpy array properties/shape.py
@@ -0,0 +1,6 @@
+# shape for find the row and column of the array
+
+import numpy as np
+arr= np.array([[2,4,3],[3,2,1],[1,2,3]]) # Creates a 3D array of shape (2, 4, 3) filled with ones
+print(arr.shape) # Prints the shape of the array
+print(arr) # Displays the array filled with ones
