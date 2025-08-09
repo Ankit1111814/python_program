95ceb917db920781335f1359df418da0cc10a856
Ankit <ankiyqq22@gmail.com>
2025-08-09 23:10:03 +0530

    fill specified value

---
+++ numpy library/full_function.py
@@ -0,0 +1,7 @@
+# full((shape), fill_value) creates an array filled with a specified value
+# shape (3) 1d, (3, 3) 2d, (3, 3, 3) 3d
+
+import numpy as np
+
+full_arr = np.full((2,2),2) # Creates a 2D array of shape (2, 2) filled with the value 3
+print(full_arr)
