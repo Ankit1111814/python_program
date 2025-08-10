8975fc1ea8eb347e7328937a5fd61a4583b8cacf
Ankit <ankiyqq22@gmail.com>
2025-08-10 12:27:09 +0530

    flatten and ravel

---
+++ numpy library/Replacing_&_manupulation/flatten.py
@@ -0,0 +1,9 @@
+# ravel = view of the array
+# flatten = copy of the array
+
+import numpy as np
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])  # Creates a 2D array
+flattened_arr = arr.flatten()  # Flattens the array
+print(flattened_arr)  # Displays the flattened array
+raveled_arr = arr.ravel()  # Ravel the array
+print(raveled_arr)  # Displays the raveled array
