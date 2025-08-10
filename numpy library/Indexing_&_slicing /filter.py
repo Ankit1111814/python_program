0109ebe46a018e9283857ede950bf52d40ea8e8e
Ankit <ankiyqq22@gmail.com>
2025-08-10 12:16:53 +0530

    filter numpy

---
+++ numpy library/Indexing_&_slicing /filter.py
@@ -0,0 +1,4 @@
+import numpy as np
+arr = np.array([3,4,2,42,2,2,4,2,2,1]) # Creates a 1D array
+print(arr[arr > 2])  # Displays elements greater than 2
+print(arr[arr < 3])  # Displays elements less than 3
