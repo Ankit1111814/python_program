324a83ac62154798e6fe7727cb59f5e100133b5f
Ankit <ankiyqq22@gmail.com>
2025-08-11 23:42:07 +0530

    change commit

--- numpy library/Replacing_&_manupulation/flatten.py
+++ numpy library/Replacing_&_manupulation/flatten.py
@@ -1,6 +1,7 @@
 # ravel = view of the array
 # flatten = copy of the array
 
+
 import numpy as np
 arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])  # Creates a 2D array
 flattened_arr = arr.flatten()  # Flattens the array
