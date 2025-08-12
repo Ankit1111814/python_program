00cbbab6ce1c7589bcdede1bbc893f1722c98a05
Ankit <ankiyqq22@gmail.com>
2025-08-12 23:09:01 +0530

    change in reshape

--- numpy library/Replacing_&_manupulation/reshape.py
+++ numpy library/Replacing_&_manupulation/reshape.py
@@ -1,5 +1,5 @@
 # reshape(rows,column)
-
+# reshape the array to the specified number of rows and columns
 import numpy as np
 arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])  # Creates a 2D array
 reshaped_arr = arr.reshape(1, 9)  # Reshapes the array
