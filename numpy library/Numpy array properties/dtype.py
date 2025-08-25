78524ad4286a73e3a4995147378006cb77b383ea
Ankit <84123819+Ankit1111814@users.noreply.github.com>
2025-08-25 22:49:44 +0530

    Update dtype.py

--- numpy library/Numpy array properties/dtype.py
+++ numpy library/Numpy array properties/dtype.py
@@ -1,4 +1,4 @@
-# dtype find the data type of the array
+# dtype find the datatype of the array
 import numpy as np
 arr = np.array([[2, 4, 3], [3, 2, 1], [1, 2, 3]])  # Creates a 2D array
 print(arr.dtype)  # Prints the data type of the elements in the array
